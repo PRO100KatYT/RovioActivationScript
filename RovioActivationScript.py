2307d4ecddb4c443bbaeddf2cdc6ad4cf3c314e7
PRO100KatYT <67335438+PRO100KatYT@users.noreply.github.com>
2026-03-06 21:03:57 +0100

    Python Script fix (v1.0.1)

--- RovioActivationScript.py
+++ RovioActivationScript.py
@@ -1,17 +1,33 @@
-import uuid, os
+import uuid, os, subprocess
 if os.name == "nt": os.system("title Rovio Activation Script")
-print("Rovio Activation Script v1.0.0 by PRO100KatYT\n")
+print("Rovio Activation Script v1.0.1 by PRO100KatYT\n")
 # Not using f-strings for compatibility with Python 3.2+
+
+def getMacAddressesInString():
+    macAddresses = []
+    try:
+        output = subprocess.check_output("getmac /fo csv /nh", universal_newlines=True)
+        for line in output.splitlines():
+            if not line.strip(): continue
+            parts = line.split(",")
+            if len(parts) > 0:
+                macAddress = parts[0].replace("\"", "").upper()
+                if len(macAddress) == 17 and macAddress.count("-") == 5:
+                    if macAddress not in macAddresses:
+                        macAddresses.append(macAddress)
+    except: pass
+    if not macAddresses: # Use the old method of getting the address just in case something goes wrong
+        mac_int = uuid.getnode()
+        macAddresses.append("-".join("{:012X}".format(mac_int)[i:i+2] for i in range(0, 12, 2)))
+    return ";".join(macAddresses)
 
 def main():
     try:
-        macAddress = '-'.join('{:012X}'.format(uuid.getnode())[i:i+2] for i in range(0, 12, 2))
-        
         xmlContent = '''<?xml version="1.0" encoding="utf-8"?>
 <data>
 <Boolean key="BDPGS12FL" value="True" />
 <String key="BDPGS12FL_hardwareID" value="{macAddress}" />
-</data>'''.format(macAddress=macAddress)
+</data>'''.format(macAddress=getMacAddressesInString())
 
         appDataRoamingPath = os.environ['APPDATA']
         appDataPath = os.path.dirname(appDataRoamingPath)
